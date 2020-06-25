5c7f37e22bdea3369b67aa5b3bbf9a8626f11e19
moh-hassan <moh.hassan20@gmail.com>
2020-06-25 19:06:06 +0200

    reset project

--- build.cmd
+++
@@ -1,5 +0,0 @@
-cd
-cmake -H. -Bbuild -G "Visual Studio 14 2015 Win64"
-cmake --build build
-rem build nuget package
-nuget pack hello.nuspec 
--- readme.txt
+++
@@ -1,8 +0,0 @@
-stage1:
-build cmake environment and build project
-stage2:
-mvn environment
-
-stage3:
-
-
